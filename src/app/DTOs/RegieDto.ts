2fe1d3acc9b9bfc6f770055c9081f08f9fd9797a
MamaneHassane <hassane.mamane017@gmail.com>
2024-05-29 16:32:37 +0100

    adding auth and register, no guards for now

--- src/app/DTOs/RegieDto.ts
+++ src/app/DTOs/RegieDto.ts
@@ -18,8 +18,47 @@ interface RegieAuthenticationResponseDto {
   accessToken: string
 }
 
+interface AdminInListAllAdminsDto {
+  adminId: number;
+  regieId: number;
+  nom: string;
+  prenom: string;
+  compteActif: boolean | null;
+  password: string;
+  dateDeNaissance: Date | string;
+  id: string;
+  userName: string;
+  normalizedUserName: string | null;
+  email: string;
+  normalizedEmail: string | null;
+  emailConfirmed: boolean;
+  passwordHash: string;
+  securityStamp: string;
+  concurrencyStamp: string;
+  phoneNumber: string | null;
+  phoneNumberConfirmed: boolean;
+  twoFactorEnabled: boolean;
+  lockoutEnd: string | null;
+  lockoutEnabled: boolean;
+  accessFailedCount: number;
+}
+
+interface AgentInListAllAgentsDto {
+  operateurId: number;
+  dateEmbauche: Date | string;
+  regieId: number;
+  civilite: string;
+  releves: any[];
+  cin: string;
+  nom: string;
+  prenom: string;
+  dateDeNaissance: string;
+}
+
 export {
   RegieAuthenticationRequestDto,
   RegieDto,
-  RegieAuthenticationResponseDto
+  RegieAuthenticationResponseDto,
+  AdminInListAllAdminsDto,
+  AgentInListAllAgentsDto
 }
--- src/app/services/regies/regies.service.ts
+++ src/app/services/regies/regies.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {regieServiceAddress} from "../../environnement";
+import {Observable} from "rxjs";
+import {AdminInListAllAdminsDto, AgentInListAllAgentsDto} from "../../DTOs/RegieDto";
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +12,20 @@ export class RegiesService {
   constructor(
     private http: HttpClient
   ) { }
+  // Lister tous les agents de terrain de la régie
+  listAllRegiesAgents(regieId: number):Observable<AgentInListAllAgentsDto[]>{
+    return this.http.get<AgentInListAllAgentsDto[]>(`${regieServiceAddress}/Regie/ListAllAgents/${regieId}`)
+  }
+  // Lister tous les administrateurs de la régie
+  listAllRegiesAdmins(regieId: number) : Observable<AdminInListAllAdminsDto[]>{
+    return this.http.get<AdminInListAllAdminsDto[]>(`${regieServiceAddress}/Regie/ListAllAdministrateurs/${regieId}`)
+  }
+  // Débloquer le compte d'un administrateur
+  unlockAdminAccount(regieId: number, adminId: number) : Observable<boolean>{
+    return this.http.post<boolean>(`${regieServiceAddress}/UnlockAdministrateur/${adminId}/${regieId}`,{})
+  }
+  // Bloquer le compte d'un administrateur
+  lockAdminAccount(regieId: number, adminId: number) : Observable<boolean>{
+    return this.http.post<boolean>(`${regieServiceAddress}/lockAdministrateur/${adminId}/${regieId}`,{})
+  }
 }
